f2dc75a2e895bcc51d514e183b2a96afa1e6c818
Eigentun <winkler2408@gmail.com>
2026-02-11 17:30:56 +0100

    error handling

--- ice-and-fire/single-character/index.js
+++ ice-and-fire/single-character/index.js
@@ -1,16 +1,16 @@
 import fs from "node:fs/promises";
 
 const getData = async (url) => {
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
-        }
-        const result = await response.json();
-        return result;
-    } catch (error) {
-        console.error(error.message);
+    //try {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
     }
+    const result = await response.json();
+    return await result;
+    // } catch (error) {
+    //     console.error(error.message);
+    // }
 };
 
 const writeFile = async (url) => {
@@ -22,8 +22,13 @@ const writeFile = async (url) => {
     );
 };
 
-// const fileWritten = () => {
-//     console.log("file written");
-// };
+const saveCharacterToFile = async () => {
+    try {
+        await writeFile("https://anapioficeandfire.com/api/characters/583");
+        console.log("success");
+    } catch (error) {
+        console.error(error.message);
+    }
+};
 
-writeFile("https://anapioficeandfire.com/api/characters/583");
+saveCharacterToFile();
